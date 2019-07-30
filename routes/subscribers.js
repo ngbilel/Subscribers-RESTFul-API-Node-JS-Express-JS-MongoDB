f44c311479443172fb5509342d43af0c298a445a
Bilel <ng.bilel@gmail.com>
2019-07-30 21:09:21 +0200

    RESTFul API - Adding Subscriber Model, Middleware & Finishing Subscribres Routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,30 +1,90 @@
 const express = require('express');
 const router = express.Router();
+const Subscriber = require('../models/subscriber')
 
 // Get All Subscribres
-router.get('/',(req,res)=>{
-    res.send('hello world')
-});
-
-// Get Subscribre by id
-router.get('/:id',(req,res)=>{
-    res.send(req.params.id)
+router.get('/', async (req,res)=>{
+    try{
+        const subscribers = await Subscriber.find();
+        res.send(subscribers);
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
 });
 
 // Add Subscribre
-router.post('/:id',(req,res)=>{
+router.post('/', async (req,res)=>{
     
+    const subscriber = new Subscriber({
+        name:req.body.name,
+        subscribedToChannel : req.body.subscribedToChannel
+    })
+
+    try {
+        const newSubscriber = await subscriber.save();
+        res.status(201).json(newSubscriber);
+
+    }catch(err){
+        res.status(400).json({message: err.message})
+    }
+});
+
+// Get Subscribre by id
+router.get('/:id',getSubscriber, async(req,res)=>{ 
+    res.send(res.subscriber);
+});
+
+
+
+//  Delete Subscribre
+router.delete('/:id',getSubscriber,async(req,res)=>{
+    try {
+        await res.subscriber.remove();
+        res.json({message: 'Subsriber Deleted'});
+
+    }catch(err){
+         res.status(500).json({message: err.message})
+    }
 });
 
 // Update Subscribre
-router.patch('/:id',(req,res)=>{
+router.patch('/:id',getSubscriber,async(req,res)=>{
 
+    let messageErrors ='';
+    if(!req.body.name) messageErrors= 'name is  require';
+    if(!req.body.subscribedToChannel ) messageErrors += ' subscribedToChannel is require';
+    if  ( messageErrors )  return res.status(400).json({message: messageErrors })
+       
+     
+    let updateSubscriber={
+        name:req.body.name,
+        subscribedToChannel:req.body.subscribedToChannel
+    }
+
+    try{
+     await res.subscriber.update(updateSubscriber)
+     res.status(200).json({message: 'Subscriber updated'})
+
+    }catch(err){
+        res.status(500).json({message : err.message})
+    }
 });
 
-// Delete Subscribre
-router.delete('/:id',(req,res)=>{
 
-});
+// Middleware
 
+async function getSubscriber(req,res,next){
+    let subscriber;
+    try {
+        subscriber = await Subscriber.findById(req.params.id)
+        if(subscriber == null){
+            return res.status(404).json({message: 'Suscriber not found'})
+        }
+    }catch(err){
+        return res.status(500).json({message : err.message})
+    }
+    res.subscriber=subscriber;
+    next()
+}
 
 module.exports = router
